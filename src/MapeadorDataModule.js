ec5c48d7ba054875881e8d26cd5b2ee10ee14666
Fabio <fabio.garbato@mps.com>
2024-03-06 14:01:10 -0300

    Ajuste de exibição dos objetos de banco

--- src/MapeadorDataModule.js
+++ src/MapeadorDataModule.js
@@ -3,6 +3,7 @@ import {Container, Row, Col, Image, Table, Button, Modal }  from 'react-bootstra
 import React, { useState, useEffect } from 'react'
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Mps from './images/mps.png'
 import Papa from 'papaparse';
 import relacaoFormsClassesObjetos from './text/relacao_forms_classes_objetos.csv'
@@ -29,16 +30,52 @@ const MapeadorDataModule = () => {
         });
       }, []);
 
-      const [filtro, setFiltro] = useState('');
+    const [filtro, setFiltro] = useState('');
 
-      const cellStyle = {
-        wordBreak: 'break-word', 
-        maxWidth: '350px', 
-        overflow: 'hidden', 
-        whiteSpace: 'normal' 
+    const [showModal, setShowModal] = useState(false);
+    const [modalContent, setModalContent] = useState('');
+
+    const cellStyle = {
+        wordBreak: 'break-word',
+        maxWidth: '350px',
+        overflow: 'hidden',
+        whiteSpace: 'normal'
+    };
+
+    const handleOpenModal = (content) => {
+        setModalContent(content);
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+  
+    const formatModalContent = (content) => {
+        const blocks = content.split(/(?:StoredProcName:|SQL.Query:)/).filter(Boolean);
+        return blocks.map((block, index) => {
+          const trimmedBlock = block.trim().replace(/\|\s*$/, ''); 
+          const prefix = index > 0 ? (trimmedBlock.startsWith('dbo.') ? 'StoredProcName: ' : 'SQL.Query: ') : '';
+          const fullText = `${prefix}${trimmedBlock}`;
+          return (
+            <div key={index} style={{ marginBottom: '10px', maxHeight: '200px', overflowY: 'auto' }}>
+                <CopyToClipboard text={fullText}
+                    onCopy={() => console.log('Texto copiado!')}>
+                        <button>
+                            <span> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="svg octicon-copy" width="16" height="16" aria-hidden="true"><path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path></svg></span>
+                        </button>
+                </CopyToClipboard>
+              <pre style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-word' }}>
+                <code>{fullText}</code>
+              </pre>
+            </div>
+          );
+        });
       };
+      
 
   return (
+    
     <Container fluid style={{ backgroundColor: 'white'}}>
         <Navbar id='inicio' expand="lg" style={{ backgroundColor: '#98FB98'}}>
             <Row className="w-100">
@@ -119,13 +156,34 @@ const MapeadorDataModule = () => {
                                     <tr>
                                         <td style={cellStyle}>{form}</td>
                                         <td style={cellStyle}>{classe}</td>
-                                        <td style={cellStyle}>{objetosTiposConcatenados || 'N/A'}</td>
+                                        <td style={cellStyle}>
+                                            <Button
+                                                variant="success"
+                                                style={{ width: '70px', height: '50px' }}
+                                                onClick={() => handleOpenModal(objetosTiposConcatenados || 'N/A')}
+                                            >
+                                                Ver
+                                            </Button>
+                                        </td>
                                     </tr>
                                 </React.Fragment>
                             );
                         })}
                 </tbody>
             </Table>
+            <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Detalhes dos Objetos de Banco</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {formatModalContent(modalContent)}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="success" onClick={handleCloseModal}>
+                        Fechar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         <Container style={{ minHeight: '10vh'}}></Container>
         <footer className="footer">
             <Container fluid >
