128a6354d2f27101f18bbf8467abde131a9afc90
Fabio <fabio.garbato@mps.com>
2024-03-08 15:49:26 -0300

    Ajuste de hospedagem do front

---
+++ src/unistall_front.js
@@ -0,0 +1,15 @@
+var Service = require('node-windows').Service;
+// Criando um novo objeto do serviço
+var svc = new Service({
+    //Nome do servico
+    name: 'MigracaoSQL-Front',
+    //Descricao que vai aparecer no Gerenciamento de serviço do Windows
+    description: 'Front do Mapeamento do HomePar',
+    //caminho absoluto do seu script
+    script: 'C:\\Projetos\\fabio.garbato\\MapeadorObjetosDelphiFront\\App.js'
+});
+svc.on('uninstall', function () {
+    console.log('Uninstall complete.');
+});
+// Desistalar serviço.
+svc.uninstall();
