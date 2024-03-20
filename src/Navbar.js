1bbd454ecef73311078f31f2816243f83271d735
fabiogarbato <82621006+fabiogarbato@users.noreply.github.com>
2024-03-19 21:32:57 -0300

    Ajuste do NavBar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,13 +2,19 @@ import { Navbar, Row, Col, Image } from 'react-bootstrap';
 import Mps from './images/mps.png'
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
 const NavBar = ({ title }) => {
     return (
-        <Navbar id='inicio' expand="lg" style={{ backgroundColor: '#98FB98', minWidth: '100vh'}}>
+        <Navbar id='inicio' expand="lg" style={{
+            backgroundColor: '#98FB98',
+            minWidth: '100vh',
+            boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)', 
+            background: 'linear-gradient(45deg, #98FB98, #4CAF50)', 
+            borderRadius: '10px',
+            opacity: '0.9' 
+        }}>
             <Row className="w-100">
                 <Col xs={12} md={4} className="d-flex justify-content-center justify-content-md-start">
                     <Navbar.Brand>
@@ -22,6 +28,7 @@ const NavBar = ({ title }) => {
                 </Col>
             </Row>
         </Navbar>
+        
     );
 };
 
